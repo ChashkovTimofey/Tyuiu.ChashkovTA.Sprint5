d04cfae0c7ccca17426b4bcaa249877cb96973d6
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 17:10:23 +0500

    Task3

---
+++ Tyuiu.ChashkovTA.Sprint5.Task3.V25.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ChashkovTA.Sprint5.Task3.V25.Lib
+{
+    public class DataService : ISprint5Task3V25
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            double result;
+
+            try
+            {
+                result = (3 * Math.Pow(x, 4) + 1) / (3 * x);
+                result = Math.Round(result, 3); 
+            }
+            catch (DivideByZeroException)
+            {
+                result = 0; 
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+
+            Console.WriteLine($"Результат: {result}");
+
+            return filePath;
+        }
+    }
+}
