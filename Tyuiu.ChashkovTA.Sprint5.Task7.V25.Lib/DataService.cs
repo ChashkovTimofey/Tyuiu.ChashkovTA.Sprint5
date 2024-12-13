67f82ffa060c26026dc5c9e2d7734e7b31a237e9
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 17:34:42 +0500

    Task7

---
+++ Tyuiu.ChashkovTA.Sprint5.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ChashkovTA.Sprint5.Task7.V25.Lib
+{
+    public class DataService : ISprint5Task7V25
+    {
+        public string LoadDataAndSave(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("Файл не найден.");
+                    return "Ошибка: файл не найден";
+                }
+
+                string[] lines = File.ReadAllLines(path);
+                var resultLines = new System.Collections.Generic.List<string>();
+                var englishWordPattern = new Regex(@"\b[a-zA-Z]+\b");
+
+                foreach (var line in lines)
+                {
+                    var modifiedLine = englishWordPattern.Replace(line, string.Empty);
+                    resultLines.Add(modifiedLine);
+                }
+
+                string tempDirectory = Path.GetTempPath();
+                string outputFilePath = Path.Combine(tempDirectory, "OutPutDataFileTask7V25.txt");
+                File.WriteAllLines(outputFilePath, resultLines);
+
+                Console.WriteLine($"Результат успешно сохранен в файл: {outputFilePath}");
+                return outputFilePath;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return "Ошибка при обработке файла";
+            }
+        }
+    }
+}
