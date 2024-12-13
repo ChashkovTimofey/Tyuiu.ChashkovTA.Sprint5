431ef64a8d334327c485d2213be5b582d3c656e9
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 16:43:21 +0500

    Task1

---
+++ Tyuiu.ChashkovTA.Sprint5.Task1.V6.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ChashkovTA.Sprint5.Task1.V6.Lib
+{
+    public class DataService : ISprint5Task1V6
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+                string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("x\tF(x)");
+
+                    for (int x = startValue; x <= stopValue; x++)
+                    {
+                        double result;
+
+                        try
+                        {
+                            result = Math.Cos(x) + (4 * x) / 2 - Math.Sin(x) * 3 * x;
+                            result = Math.Round(result, 2); 
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            result = 0; 
+                        }
+
+                        writer.WriteLine($"{x}\t{result}");
+
+                        Console.WriteLine($"{x}\t{result}");
+                    }
+                }
+
+                return $"Результаты сохранены в файл: {filePath}";
+            }
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint5.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.ChashkovTA.Sprint5.Task1.V6.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
